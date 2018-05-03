0698f6fdd99dcceed13e8c4c373b425c33586381
Asif Iqubal <asif633@gmail.com>
2018-05-03 16:39:50 +0530

    Added Angular PWA Project

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'app';
+  newsitems: any[];
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.httpClient.get('http://contextualwebsearch.com/api/Search/NewsSearchAPI?q=angular&count=50&autoCorrect=true')
+    .subscribe((data:any) => this.newsitems = data.value);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { AppComponent } from './app.component';
+
+import { environment } from '../environments/environment';
+import { AppRoutingModule } from './router.module';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
